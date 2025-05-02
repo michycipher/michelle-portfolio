15f0fef26f11d7cfcbb5ded0d7b83b293caf0a78
michycipher <michelleutomi@yahoo.com>
2025-05-02 11:47:53 +0100

    portfolio image display

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,36 +7,24 @@ const items = [
   {
     id: 1,
     title: "InnoX",
-    img: "/innox.png",
+    img: "./images/innox.png",
     desc: "InnoX Africa, an innovation platform designed for challenges, hackathons, and venture building across Africa. I developed a responsive, high-performance frontend that ensures seamless navigation, integrates powerful engines, and scales for future growth, all optimized to support innovators and organizations in creating and scaling solutions.",
     link: "https://www.innox.africa/",
   },
   {
     id: 2,
     title: "Akwa Ibom Tech week",
-    img: "/AKTW.png",
+    img: "./images/AKTW.png",
     desc: "AKTW on InnoX Africa is a professional innovation platform offering tailored tools for collaboration and growth. Its responsive, high-performing design ensures seamless user experiences and intuitive workflows. Built for scalability, it supports professionals and organizations in driving impactful innovation.",
     link: "https://aktw.innox.africa/",
   },
   {
     id: 3,
     title: "Ubulu Africa",
-    img: "/ubulu.png",
+    img: "./images/ubulu.png",
     desc: "Ubulu Africa is a digital innovation hub built with Next.js, React.js, and Tailwind CSS. It features a fast, responsive, and SEO-optimized frontend focused on performance and scalability. The platform empowers innovators and organizations to connect, collaborate, and scale solutions across Africa.",
   link: "https://ubulu.africa/",
   },
-  // {
-  //   id: 4,
-  //   title: "Music App",
-  //   img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  // },
-  // {
-  //   id: 5,
-  //   title: "Music App",
-  //   img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  // },
 ]
 
 const Single = ({ item }) => {
